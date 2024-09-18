4c66d2b67c5c69521b2a4068676c836e112f788d
Rudy N. Caña <rudy_cana@cody.inc>
2024-09-18 12:21:37 +0800

    Added test case 12

---
+++ tests/agent/YDN9-114/37_AGENT_UC012_TC012.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+const ID = "111034005001";
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/dealer-info/login.php');
+    await page.locator('input[name="charge_id"]').fill(process.env.USERID);
+    await page.locator('input[name="password"]').fill(process.env.PASSWORD);
+    await page.locator('#login_button').click();
+});
+
+test('Redirect to User Information System Usage Item Deletion Request Page', async({ page })=> {
+    //#region STEPS
+    await page.goto('/dealer-info/idm_user/menu.php');
+    await page.locator('a[href="./search.php"]').click();
+    await page.locator(`input[onclick*="refer.php?id=${ID}"]`).click();
+    await page.locator(`input[onclick*="delete_system_input.php?id=${ID}"]`).click();
+    await page.locator('input[name="keyboard"]').check();
+    await page.getByRole('button', { name: '入力内容を確認する' }).click();
+    //#endregion
+
+    //#region ASSERTIONS
+    expect(page.url()).toBe(`${process.env.BASEURL}/dealer-info/idm_user/delete_system_confirm.php`);
+    await expect(page.getByRole('link', { name: 'Yamaha Dealers Net' })).toBeVisible();
+    //#endregion
+});
